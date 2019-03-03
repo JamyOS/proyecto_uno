071efb27bfc0eb99afe8cffbb8b35bdae1841c0d
Luzop <genkishot@gmail.com>
2019-03-03 12:56:09 -0600

    Calculadora Kotlin v1

--- calc.kt
+++ calc.kt
@@ -1,36 +1,46 @@
-fun sum(a: Int, b: Int): Int {
+fun sum(a: Double, b: Double): Double {
     return a + b
 }
 
-fun rest(a: Int, b: Int): Int {
+fun rest(a: Double, b: Double): Double {
     return a - b
 }
 
-fun mult(a: Int, b: Int): Int {
+fun mult(a: Double, b: Double): Double {
     return a * b
 }
 
-fun div(a: Int, b: Int): Int {
+fun div(a: Double, b: Double): Double {
     return a / b
 }
 
 fun main(){
-    var a: Int 
-    var b: Int 
+    var a: Double
+    var b: Double 
+    var r: Int
+    
+    do{
+       println("Ingrese dos numeros")
+       a = readLine()?.toDouble() as Double
+       b = readLine()?.toDouble() as Double
 
-    println("Ingrese dos numeros")
-    a = readLine()?.toInt() as Int
-    b = readLine()?.toInt() as Int
+       println("La suma de $a y $b es")
+       println(sum(a, b))
 
-    println("La suma de $a y $b es")
-    println(sum(a, b))
+       println("La resta de $a y $b es")
+       println(rest(a, b))
 
-    println("La resta de $a y $b es")
-    println(rest(a, b))
+       println("La multiplicacion de $a y $b es")
+       println(mult(a, b))
 
-    println("La multiplicacion de $a y $b es")
-    println(mult(a, b))
-
-    println("La divicion de $a y $b es")
-    println(div(a, b))
+       if (b == 0.0){
+          println("No es posible dividir sobre 0")
+       }
+       else{
+          println("La divicion de $a y $b es")
+          println(div(a, b))
+       }
+      println ("¿Desea realizar otra operacion? (1/0)")
+      r = readLine()?.toInt() as Int
+   } while (r == 1)
 }
